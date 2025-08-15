98ac5dc112c8b71ca30d45d3b352ba107b30b2da
MD. Rafiul Islam <rafiulislam316@gmail.com>
2025-08-16 01:08:12 +0600

    Create full virsion of summetion.c
    oalid bro help me

---
+++ 13-aug/full virsion of summetion.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+
+int main()
+{
+    int P, R,sum=0;
+    
+    printf(" Enter the value of 1st number:");
+    scanf("%d",&P);
+    printf(" Enter the value of 2nd number :");
+    scanf("%d",&R);
+   if(P<R) {
+       for(int i=P;i<R;i++){
+       sum = sum + i;
+    printf("%d + ",i);
+    }
+    printf("%d = %d",R,sum+R);
+   }
+   else if (R<P){
+       for(int i=P;i>R;i--){
+       sum = sum + i;
+    printf("%d + ",i);
+    }
+    printf("%d = %d",R,sum+R);
+   }
+    return 0;
+}
