83ca917853ffa25ed9da1f7872e3bad3ffb3cd3b
MD. Rafiul Islam <rafiulislam316@gmail.com>
2025-08-29 21:11:20 +0600

    Update sum of digits.c

--- 13-aug/sum of digits.c
+++ 13-aug/sum of digits.c
@@ -1,13 +1,11 @@
-// Online C compiler to run C program online
 #include <stdio.h>
 
 int main() {
-    int A,B,c,sum=0;
-    printf("Enter numbers of digit:");
-    scanf("%d",&A);
+    int B,c,sum=0;
+   
     printf("Enter the digit:");
     scanf("%d",&B);
-    for(int i=1 ; i<=A;i++){
+    for(; B%10>0;){
         c=B%10;
         sum=sum+c;
         B/=10;
