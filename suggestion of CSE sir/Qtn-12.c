bf1ed1fd683b273a59f86915275a6cf433ce3dad
MD. Rafiul Islam <rafiulislam316@gmail.com>
2025-09-04 17:02:23 +0600

    Create Qtn-12.c
    12.Write a C program to input a number and check positive negative or zero using
    switch case.

---
+++ suggestion of CSE sir/Qtn-12.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    int N,count=0;
+    scanf("%d",&N);
+    switch (N>0) {
+        case 1 : printf("positive Number");
+        break;
+        case 0 : printf("negitive  Number");
+        break;
+
+    }
+
+    return 0;
+}
