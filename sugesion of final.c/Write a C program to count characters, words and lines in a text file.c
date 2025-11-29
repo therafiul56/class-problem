cfd6781a2f5d92be39d6e472fc9ba5991baf2cc0
MD. Rafiul Islam <rafiulislam316@gmail.com>
2025-11-30 01:13:50 +0600

    Add character, word, and line counter program
    This program counts the number of characters, words, and lines in a specified text file.

---
+++ sugesion of final.c/Write a C program to count characters, words and lines in a text file.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+int main() {
+    FILE *fp;
+    char filename[100];
+    char ch;
+    int characters = 0, words = 0, lines = 0;
+    int inWord = 0;
+
+    printf("Enter file name: ");
+    scanf("%s", filename);
+
+    fp = fopen(filename, "r");
+
+    if (fp == NULL) {
+        printf("Error: Cannot open file.\n");
+        return 1;
+    }
+
+    while ((ch = fgetc(fp)) != EOF) {
+        characters++;
+
+        if (ch == '\n')
+            lines++;
+
+        if (ch == ' ' || ch == '\t' || ch == '\n')
+            inWord = 0;
+        else if (inWord == 0) {
+            inWord = 1;
+            words++;
+        }
+    }
+
+    fclose(fp);
+
+    printf("Characters: %d\n", characters);
+    printf("Words: %d\n", words);
+    printf("Lines: %d\n", lines);
+
+    return 0;
+}
