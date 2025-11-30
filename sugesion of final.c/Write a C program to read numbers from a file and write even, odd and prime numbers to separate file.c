929843169365ccdad35dd22b947ed38b561cb00e
MD. Rafiul Islam <rafiulislam316@gmail.com>
2025-11-30 17:44:07 +0600

    Refactor number categorization program
    Updated the program to read numbers from a file and write even, odd, and palindrome numbers to separate files. Removed prime number functionality and added palindrome check.

--- sugesion of final.c/Write a C program to read numbers from a file and write even, odd and prime numbers to separate file.c
+++ sugesion of final.c/Write a C program to read numbers from a file and write even, odd and prime numbers to separate file.c
@@ -1,49 +1,52 @@
 #include <stdio.h>
-#include <math.h>
 
-int isPrime(int n) {
-    if (n < 2) return 0;
-    for (int i = 2; i <= sqrt(n); i++) {
-        if (n % i == 0) return 0;
+int chek(int num)
+{
+    int original = num;
+    int rev = 0;
+
+    while(num != 0){
+        rev = rev * 10 + (num % 10);
+        num /= 10;
     }
-    return 1;
+
+    return (original == rev);
 }
 
-int main() {
-    FILE *input, *evenFile, *oddFile, *primeFile;
+int main()
+{
+    FILE *fp_in;
+    FILE *fp_even, *fp_odd, *fp_pal;
+
+    char filename[1000] = "chudi.txt";
+
+    fp_in = fopen(filename, "r");
+
+    if(fp_in == NULL){
+        printf("ERROR: the file cannot be opened\n");
+        return 1;
+    }
+
+    fp_even = fopen("even.txt", "a");
+    fp_odd  = fopen("odd.txt", "a");
+    fp_pal  = fopen("pal.txt",  "a");
+
     int num;
-
-    input = fopen("numbers.txt", "r");
-    if (input == NULL) {
-        printf("Error: Cannot open input file.\n");
-        return 1;
-    }
-
-    evenFile = fopen("even.txt", "w");
-    oddFile = fopen("odd.txt", "w");
-    primeFile = fopen("prime.txt", "w");
-
-    if (evenFile == NULL || oddFile == NULL || primeFile == NULL) {
-        printf("Error: Cannot create output files.\n");
-        return 1;
-    }
-
-    while (fscanf(input, "%d", &num) != EOF) {
-        if (num % 2 == 0)
-            fprintf(evenFile, "%d\n", num);
+    while (fscanf(fp_in, "%d", &num) != EOF)
+    {
+        if(num % 2 == 0)
+            fprintf(fp_even, "%d\n", num);
         else
-            fprintf(oddFile, "%d\n", num);
+            fprintf(fp_odd, "%d\n", num);
 
-        if (isPrime(num))
-            fprintf(primeFile, "%d\n", num);
+        if(chek(num))
+            fprintf(fp_pal, "%d\n", num);
     }
 
-    fclose(input);
-    fclose(evenFile);
-    fclose(oddFile);
-    fclose(primeFile);
-
-    printf("Numbers separated successfully.\n");
+    fclose(fp_in);
+    fclose(fp_even);
+    fclose(fp_odd);
+    fclose(fp_pal);
 
     return 0;
 }
