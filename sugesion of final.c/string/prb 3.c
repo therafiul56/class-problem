30991f3647d8a3d6c13c168ac2d7189fd22ee9b2
MD. Rafiul Islam <rafiulislam316@gmail.com>
2025-12-02 13:29:06 +0600

    Improve word counting in input string
    Refactor word counting logic to handle in-word state.

--- sugesion of final.c/string/prb 3.c
+++ sugesion of final.c/string/prb 3.c
@@ -2,12 +2,16 @@
 #include<string.h>
 int main() {
     char str[1000];
-    int word=0;
+    int word=0,inword = 0;
     fgets(str,sizeof(str),stdin);
     for(int i = 0 ; str[i]!= '\0' ; i++){
         if(str[i]==' '||str[i]=='\t'||str[i]=='\n'){
-            word++;
+            inword=0;
         }
+		else if(inword == 0){
+			inword++;
+			word++;
+		}
     }
 	printf("word = %d",word);
     return 0;
